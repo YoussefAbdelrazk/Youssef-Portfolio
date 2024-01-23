ea0bad526498d9211cbac21f646888fd09f86663
= <youssefabdelrazk2002@gmail.com>
2024-01-23 03:47:19 +0200

    first commit

---
+++ src/data/projectsList.tsx
@@ -0,0 +1,179 @@
+import Comfystoreweb from "../assets/SCR-20240123-dqhq.jpeg";
+import Foodweb from "../assets/SCR-20240123-doby.jpeg";
+import Sloth_ecommerceWeb from "../assets/SCR-20240123-drck.jpeg";
+// import cmsWEBP from "../assets/cms.webp";
+// import studySphereWEBP from "../assets/studySphere.webp";
+// import dreamRestaurantWEBP from "../assets/dreamRestaurantPNG.webp";
+
+import Comfystore from "../assets/SCR-20240123-dqhq.jpeg";
+import Food from "../assets/SCR-20240123-doby.jpeg";
+import Sloth_ecommerce from "../assets/SCR-20240123-drck.jpeg";
+// import cmsPNG from "../assets/cms.png";
+// import studySpherePNG from "../assets/studySphere.png";
+// import dreamRestaurantPNG from "../assets/dreamRestaurantPNG.png";
+
+export const projectsList = [
+  {
+    id: 1,
+    title: "Comfy Store",
+    description:
+  `The Comfy Store Project aimed to develop an online e-commerce platform tailored for users
+  seeking comfort-focused products, ranging from apparel to home essentials with darkmode`,
+    webpSrc: Comfystoreweb,
+    pngSrc: Comfystore,
+    imageHeight: 800,
+    link: "https://nimble-peony-f88b0f.netlify.app/",
+    github: "https://github.com/YoussefMohamedHazo/storeProject",
+    collaborators: [
+      {
+        username: "MrGhost23",
+        image: "https://avatars.githubusercontent.com/u/72778139",
+      },
+    ],
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      
+      "React",
+      "React Router",
+      "Redux Toolkit",
+      "daisy ui",
+      "Tailwind",
+      "Axios",
+    ],
+  },
+  {
+    id: 2,
+    title: "Food Website",
+    description:
+    `Delicious Bites is a premier online destination for food enthusiasts, offering a rich blend of
+    recipes, culinary tips, restaurant reviews, and more.`,
+    webpSrc: Foodweb,
+    pngSrc: Food,
+    imageHeight: 1100,
+    link: "https://euphonious-horse-205301.netlify.app/",
+    github: "https://github.com/YoussefMohamedHazo/FoodApp",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "React Router",
+      "Tailwind",
+    ],
+  },
+  {
+    id: 3,
+    title: "Sloth E-commerce",
+    description:
+      `Sloth E-commerce is a digital platform designed for individuals seeking a laid-back and
+      stress-free online shopping experience. Drawing inspiration from the calm and unhurried nature
+      of sloths, the platform aims to simplify the e-commerce process while offering a curated
+      selection of products.`,
+    webpSrc: Sloth_ecommerceWeb,
+    pngSrc: Sloth_ecommerce,
+    imageHeight: 1100,
+    link: "https://ornate-nougat-f491c5.netlify.app/",
+    github: "https://github.com/YoussefMohamedHazo/sloth-store",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "React Router",
+      "Axios",
+    ],
+  },
+  // {
+  //   id: 4,
+  //   title: "Dream Restaurant",
+  //   description:
+  //     "An innovative and customer-centric restaurant website, designed to enhance the dining experience for both in-house and remote patrons. Our website offers a seamless ordering and delivery system, as well as a convenient table reservation feature.",
+  //   webpSrc: dreamRestaurantWEBP,
+  //   pngSrc: dreamRestaurantPNG,
+  //   imageHeight: 900,
+  //   link: "https://dream-restaurant.netlify.app",
+  //   github: "https://github.com/0xOmarAdel/dream",
+  //   collaborators: [
+  //     {
+  //       username: "MrGhost23",
+  //       image: "https://avatars.githubusercontent.com/u/72778139",
+  //     },
+  //     {
+  //       username: "noahwillson",
+  //       image: "https://avatars.githubusercontent.com/u/79482929",
+  //     },
+  //   ],
+  //   tags: [
+  //     "HTML",
+  //     "CSS",
+  //     "Javascript",
+  //     "React",
+  //     "Tailwind",
+  //     "Daisy UI",
+  //     "Framer Motion",
+  //     "Formik",
+  //     "Yup",
+  //     "Axios",
+  //     "Redux Toolkit",
+  //     "Node JS",
+  //     "Express",
+  //     "Mongo DB",
+  //   ],
+  // },
+  // {
+  //   id: 5,
+  //   title: "Complaint Management System",
+  //   description:
+  //     "The Complaint Management System is a web-based application designed to help organizations efficiently handle and track customer complaints and feedback. This system streamlines the process of receiving, documenting, and resolving complaints, ensuring a better customer experience and improved service quality.",
+  //   webpSrc: cmsWEBP,
+  //   pngSrc: cmsPNG,
+  //   imageHeight: 500,
+  //   link: "https://cms-complaints-app.netlify.app",
+  //   github: "https://github.com/0xOmarAdel/cms",
+  //   collaborators: [
+  //     {
+  //       username: "MrGhost23",
+  //       image: "https://avatars.githubusercontent.com/u/72778139",
+  //     },
+  //   ],
+  //   tags: [
+  //     "HTML",
+  //     "CSS",
+  //     "Javascript",
+  //     "React",
+  //     "React Router",
+  //     "Redux Toolkit",
+  //     "Tailwind",
+  //     "Axios",
+  //     "Node JS",
+  //     "Express",
+  //     "Mongo DB",
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   title: "Study Sphere",
+  //   description:
+  //     "Study Sphere is a platform designed for educational purposes. The app includes sections such as a hero banner, an about page, a teachers section, courses page, and a contact section using the emailjs library for email functionality.",
+  //   webpSrc: studySphereWEBP,
+  //   pngSrc: studySpherePNG,
+  //   imageHeight: 1300,
+  //   link: "https://study-sphere.netlify.app",
+  //   github: "https://github.com/0xOmarAdel/study-sphere",
+  //   tags: [
+  //     "HTML",
+  //     "CSS",
+  //     "Javascript",
+  //     "React",
+  //     "Chakra UI",
+  //     "Framer Motion",
+  //     "Formik",
+  //     "Yup",
+  //     "Typed JS",
+  //     "Swiper JS",
+  //     "Google Map React",
+  //   ],
+  // },
+];
